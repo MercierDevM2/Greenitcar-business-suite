f8e24ce09470d5e61a96e670a9ac1cf05f8e5ee6
Mercier_Dev <merciergoni@gmail.com>
2026-07-13 18:00:44 +0100

    fix: standalone et serverMinification pour erreur lstat

--- next.config.ts
+++ next.config.ts
@@ -6,14 +6,19 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  // Regroupement des options Workbox pour éviter l'erreur de type
   workboxOptions: {
     exclude: [/middleware-manifest\.json$/, /page_client-reference-manifest\.js$/],
   }
 });
 
 const nextConfig: NextConfig = {
-  //Config...
+  // 1. Force Vercel à l'indépendance de build et à l'unification des manifests
+  output: "standalone",
+  
+  experimental: {
+    // 2. Désactive la minification serveur pour empêcher la perte des fichiers lstat client-reference
+    serverMinification: false,
+  }
 };
 
 export default withPWA(nextConfig);
