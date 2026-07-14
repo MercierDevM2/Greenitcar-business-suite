6090fdabdd4487bda0f4668b734282596d266b17
Mercier_Dev <merciergoni@gmail.com>
2026-07-14 13:57:18 +0100

    Nettoyage

--- app/(public)/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PublicHomePage() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>GreenITCar</h1>
-      <p>Bienvenue sur la plateforme vitrine GreenITCar.</p>
-    </main>
-  );
-}
